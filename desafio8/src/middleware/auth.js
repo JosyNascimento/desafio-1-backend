eec5a55ad7c20d5b54e97850ff451f2881541fb2
Joseilda Nascimento <joseildatn@gmail.com>
2025-01-14 02:42:09 -0300

    corrigindo erros de rotas

---
+++ desafio8/src/middleware/auth.js
@@ -0,0 +1,14 @@
+// authenticate.js
+const authenticate = (req, res, next) => {
+    console.log(req.session?.user); 
+    // Verifique a autenticação, como um token JWT ou sessão
+    if (req.session?.user) {
+      req.user = req.session.user;  // Defina req.user com as informações do usuário
+      return next();  // Se autenticado, continue para o próximo middleware
+    }
+  
+    return res.status(401).json({ message: 'Usuário não autenticado' });
+  };
+  
+  module.exports = authenticate;
+  
