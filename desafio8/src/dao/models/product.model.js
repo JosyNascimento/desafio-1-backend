a7862a8300879407741f397162314d8bfe78266e
Joseilda Nascimento <64994809+JosyNascimento@users.noreply.github.com>
2025-01-14 22:35:40 -0300

    Update product.model.js

--- desafio8/src/dao/models/product.model.js
+++ desafio8/src/dao/models/product.model.js
@@ -1,3 +1,5 @@
+//product.model.js
+
 const mongoose = require('mongoose');
 
 // Definindo o esquema de um produto
@@ -5,6 +7,11 @@ const productSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
+  category: { type: String, required: true },
+  stock: { type: Number, default: 0 },
+  image: { type: String },
 });
 
-module.exports = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
