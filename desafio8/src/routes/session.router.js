0a433ed627696aa0f891e9ef10603ae963266000
Joseilda Nascimento <64994809+JosyNascimento@users.noreply.github.com>
2025-01-14 22:11:51 -0300

    Update session.router.js

--- desafio8/src/routes/session.router.js
+++ desafio8/src/routes/session.router.js
@@ -1,30 +1,59 @@
-// routes/session.router.js
+// Desafio8/src/routes/session.JSON
 const express = require('express');
 const router = express.Router();
-const User = require('../dao/mongodb/user.model');
+const User = require('../dao/models/user.model');
 const bcrypt = require('bcrypt');
 
-// Rota para Login
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  
-  const user = await User.findOne({ email });
-  
-  if (!user || !(await user.comparePassword(password))) {
-    return res.status(400).send('Email ou senha incorretos');
-  }
+// Usuário admin hardcoded
+const ADMIN_EMAIL = 'adminCoder@coder.com';
+const ADMIN_PASSWORD = 'adminCod3r123';
 
-  req.session.user = user; // Armazena os dados do usuário na sessão
-  res.redirect('/products'); // Redireciona para a página de produtos após o login
+// Middleware para adicionar o usuário à variável locals
+router.use((req, res, next) => {
+  if (req.session.user) {
+    res.locals = { user: req.session.user };
+  };
+  next();
 });
 
-// Rota para Logout
-router.get('/logout', (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      return res.status(500).send('Erro ao encerrar a sessão');
+// Rota para renderizar a página de login
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Login' });
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Substitua esta lógica pela validação que você configurou no seu projeto
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).send('Usuário não encontrado');
     }
-    res.redirect('/login');
+
+      // Compara a senha inserida com a armazenada (hash)
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).send('Senha incorreta');
+      }
+
+    // Autenticação bem-sucedida
+    res.send('Login bem-sucedido');
+  } catch (error) {
+    console.error('Erro ao realizar login:', error);
+    res.status(500).send('Erro interno no servidor');
+  }
+});
+
+// Rota para logout
+router.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (!err) {
+      res.send('Logout efetuado com sucesso!');
+    } else {
+      res.send({ status: 'Erro no logout', body: err });
+    }
   });
 });
 
